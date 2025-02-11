0e939f65036c4e17d7f29d21d2488b4e8881d243
Cersei <bestkarmarussia@gmail.com>
2025-02-11 12:48:30 +0300

    games api readded

--- app/api/routes.py
+++ app/api/routes.py
@@ -3,14 +3,14 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.future import select
 from sqlalchemy.sql import func
 from app.bd.database import get_db
-from app.bd.models import Viewer
+from app.bd.models import Viewer, Game
 from typing import List
 from pydantic import BaseModel
 
 from fastapi_cache.decorator import cache # GPT!  
-from fastapi_cache import FastAPICache
-from fastapi_cache.backends.redis import RedisBackend
-from redis import asyncio as aioredis
+from fastapi_cache import FastAPICache #gpt
+from fastapi_cache.backends.redis import RedisBackend #gpt
+from redis import asyncio as aioredis #gpt
 
 router = APIRouter()
 
@@ -24,6 +24,20 @@ async def startup():
 class ViewerResponse(BaseModel):
     total_viewers: int
     date: str
+
+class GamesResponse(BaseModel):
+    game_id:int
+    game_name:str
+
+@router.get("/games", response_model=List[GamesResponse])
+async def get_games(db: AsyncSession = Depends(get_db)):
+    result = await db.execute(
+        select(
+            Game.game_id.label("game_id"),
+            Game.game_name.label("game_name")
+        )
+    )
+    return result.mappings().all()
 
 @router.get("/viewers", response_model=List[ViewerResponse])
 @cache(expire=300)  # 300 секунд = 5 минут GPT!!
